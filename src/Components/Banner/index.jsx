6a79cd30a1cf00b88e081bb4d27c8ce8faad691e
Goro-K <69romankizil69@gmail.com>
2022-10-25 14:23:05 +0200

    Banner in progress

---
+++ src/Components/Banner/index.jsx
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return(
+        <section className='banner-accueil banner-aboutus'>
+            <div>
+                <span className='banner-span'>Chez vous, partout et ailleurs</span>
+            </div>
+        </section>
+    )
+}
