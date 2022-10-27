c650866d8901c85382efee838cc00cf0637732f1
Goro-K <69romankizil69@gmail.com>
2022-10-27 13:02:30 +0200

    add export Banner  and add a classname

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -3,9 +3,11 @@ import './Banner.css'
 function Banner() {
     return(
         <section className='banner-accueil banner-aboutus'>
-            <div>
+            <div className='banner-img'>
                 <span className='banner-span'>Chez vous, partout et ailleurs</span>
             </div>
         </section>
     )
 }
+
+export default Banner
