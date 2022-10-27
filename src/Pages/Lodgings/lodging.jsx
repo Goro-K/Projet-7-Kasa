6b20f68eba32cc474257e575f4e68fa15bb1a0a1
Goro-K <69romankizil69@gmail.com>
2022-10-27 13:04:32 +0200

    change some comment/text

--- src/Pages/Lodgings/lodging.jsx
+++ src/Pages/Lodgings/lodging.jsx
@@ -3,10 +3,10 @@ import Lodgings from '../../logements.json'
 import Error from '../../Components/Error/index'
 
 function Lodging() {
-    // 👇️ get ID from url
+    // get ID from url
     const params = useParams();
     const LodgingId = Lodgings.map(lodging => lodging.id)
-    return (LodgingId.includes(params.id) ? <h2>userId is 👉️ {params.id}</h2> : <Error />)
+    return (LodgingId.includes(params.id) ? <h2>userId is {params.id}</h2> : <Error />)
 }
 
 export default Lodging
