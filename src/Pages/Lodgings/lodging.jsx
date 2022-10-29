7df5f5c85ee1e5131bb3a0a84439cd4f5c89e3d9
Goro-K <69romankizil69@gmail.com>
2022-10-29 18:52:34 +0200

    add page logic for get only one lodging with filter

--- src/Pages/Lodgings/lodging.jsx
+++ src/Pages/Lodgings/lodging.jsx
@@ -1,12 +1,30 @@
 import {useParams} from 'react-router-dom'
 import Lodgings from '../../logements.json'
-import Error from '../../Components/Error/index'
+import Error from '../Error/index'
 
 function Lodging() {
     // get ID from url
     const params = useParams();
     const LodgingId = Lodgings.map(lodging => lodging.id)
-    return (LodgingId.includes(params.id) ? <h2>userId is {params.id}</h2> : <Error />)
+
+    const lodgingPage = (Lodgings.filter((lodging) => lodging.id === params.id))
+
+    return (
+        <div>
+            {LodgingId.includes(params.id) ?
+                lodgingPage.map(lodging => (
+                    <div>
+                        <div className='slideshow'>
+                        </div>
+                        <div> 
+                            <h2>{lodging.title}</h2>
+                        </div>
+                    </div>
+
+                ))
+            : <Error /> }   
+        </div> 
+    )    
 }
 
 export default Lodging
