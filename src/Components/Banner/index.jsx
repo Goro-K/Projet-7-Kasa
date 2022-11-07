fd49ec10229de4afc06d8af047c3b2af28079898
Goro-K <69romankizil69@gmail.com>
2022-11-07 12:39:17 +0100

    change the logic of condition to a logic of add class with props

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -1,14 +1,13 @@
 import './Banner.css'
 import { useLocation } from 'react-router-dom'
 
-function Banner() {
+function Banner({classAboutUs, classSpanAboutUs}) {
     const { pathname } = useLocation()
     return(
         <section className='banner'>
-            {pathname === "/Home" ? 
-            <div className='banner-img-home'>
-                <span className='banner-span'>Chez vous, partout et ailleurs</span>
-            </div> : <div className='banner-img-aboutus'></div>} 
+            <div className={`banner-img-home ${classAboutUs}`}>
+                <span className={`banner-span ${classSpanAboutUs}`}>Chez vous, partout et ailleurs</span>
+            </div>
 
         </section>
     )
