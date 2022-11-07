410ab0099dc8ac0e94885e9fc471337e84c02beb
Goro-K <69romankizil69@gmail.com>
2022-11-07 12:43:35 +0100

    add some class used with props

--- src/Pages/About Us/aboutUs.jsx
+++ src/Pages/About Us/aboutUs.jsx
@@ -1,18 +1,24 @@
 import Banner from '../../Components/Banner/index'
 import Collapse from '../../Components/Collapse/index'
 import Infos from '../../info.json'
-
+import './AboutUs.css'
 function aboutUs() {
     return (
         <div>
-            <Banner />
-            {Infos.map(info => (
-                <Collapse 
-                title = {info.title}
-                detail = {info.detail}
-                />
-            ))
-            }
+            <Banner 
+            classAboutUs="banner-img-aboutus"
+            classSpanAboutUs="span-aboutus"
+            />
+            <div className='collapse-container'>
+                {Infos.map(info => (
+                    <Collapse
+                    key = {info.title} 
+                    title = {info.title}
+                    detail = {info.detail}
+                    />
+                ))
+                }
+            </div>
         </div>
     )
 }
