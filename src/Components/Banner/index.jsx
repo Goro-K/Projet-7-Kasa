110262e5944c254119f9aed8388732c769be487f
Goro-K <69romankizil69@gmail.com>
2022-11-09 14:18:48 +0100

    add a condition

--- src/Components/Banner/index.jsx
+++ src/Components/Banner/index.jsx
@@ -4,9 +4,9 @@ import { useLocation } from 'react-router-dom'
 function Banner({classAboutUs, classSpanAboutUs}) {
     const { pathname } = useLocation()
     return(
-        <section className='banner'>
-            <div className={`banner-img-home ${classAboutUs}`}>
-                <span className={`banner-span ${classSpanAboutUs}`}>Chez vous, partout et ailleurs</span>
+        <section className="banner">
+            <div className={pathname === "/Home" ? "banner-img-home" : classAboutUs }>
+                <span className={pathname === "/Home" ? "banner-span" : classSpanAboutUs }>Chez vous, partout et ailleurs</span>
             </div>
 
         </section>
