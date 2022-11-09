313b38d8c9d1c9352719078dd1a75971006702e4
Goro-K <69romankizil69@gmail.com>
2022-11-09 11:55:46 +0100

    add the logic of slideshow

--- src/Components/Slideshow/index.jsx
+++ src/Components/Slideshow/index.jsx
@@ -1,8 +1,24 @@
 import './Slideshow.css'
-function Slideshow(picture) {
+import Vector from '../../image/Vector.png'
+import { useState } from 'react'
+
+function Slideshow(pictures) {
+
+    let [count, setCount] = useState(0)
+    const picturesArray = pictures.pictures
+
     return (
-        <div className='slide'>
-            <img src={picture.picture} alt="" className="slide-img"/>
+        <div className='slideshow'>
+            {picturesArray.length > 1 ? <img src={Vector} className='left-button' onClick={() => setCount(count - 1)} /> : null } 
+            
+            <img src={picturesArray[count]} className="slide-img" />
+            {count >= picturesArray.length ? setCount(0) :
+             count < 0 ? setCount(picturesArray.length - 1) 
+             : null 
+            }
+
+            {picturesArray.length > 1 ? <img src={Vector} className='right-button' onClick={() => setCount(count + 1)} /> : null}
+            {picturesArray.length > 1 ? <span className='slide-number'>{`${count + 1}/${picturesArray.length}`}</span> : null }
         </div>
     )
 }
