5629188e8fae435f19bcb7ab2d461904a8b56c10
Goro-K <69romankizil69@gmail.com>
2022-11-09 15:10:52 +0100

    add /error when it's a non-existent url

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/home/home'
 import AboutUs from './pages/aboutus/aboutUs'
 import Header from './components/header'
@@ -10,6 +10,7 @@ import './container.css'
 import Lodging from './pages/lodgings/lodging'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -17,10 +18,11 @@ root.render(
         <div className='container-2'>
         <Header />
         <Routes>
-          <Route path="/home" element={<Home />}/>
+          <Route path="/home" element={<Home />} />
           <Route path="/lodging/:id" element={<Lodging />} />
           <Route path="/aboutus" element={<AboutUs />}/>
-          <Route path="/error" element={<Error />}/>
+          <Route path="*" element={<Navigate to="/error" />} />
+          <Route path="/error" element={<Error />} />
         </Routes>
         </div>
       </div>
